080bd87d666cadcb3b3413ab966e8b7c2832dc51
Daniil Penkin <dpenkin@atlassian.com>
2020-10-04 00:59:19 +1000

    Support order by multiple fields
    Add `orderBy()` method that takes multiple fields as arguments.

--- src/main/kotlin/com/atlassian/jira/jql/Clause.kt
+++ src/main/kotlin/com/atlassian/jira/jql/Clause.kt
@@ -13,13 +13,18 @@ open class Clause(private val jql: String) : JqlEntity {
     infix fun or(clause: () -> Clause): Clause =
         CompoundClause(this, Keyword.OR, clause.asClause())
 
-    // TODO support multi order
-    infix fun orderBy(fieldWithOrder: FieldOrder): String =
+    infix fun orderBy(fieldWithOrder: FieldOrder): String = orderBy(listOf(fieldWithOrder))
+
+    infix fun orderBy(fieldsWithOrder: List<FieldOrder>): String =
         toJql()
             // In case the clause to which the order is applied to is effectively empty,
             // the resulting JQL string should be empty as well
             .takeIf { it.isNotBlank() }
-            ?.let { "$it order by ${fieldWithOrder.toJql()}" }
+            ?.let { clause ->
+                fieldsWithOrder.joinToString(separator = ", ") { it.toJql() }
+                    .takeIf { it.isNotBlank() }
+                    ?.let { orderBy -> "$clause ORDER BY $orderBy" }
+            }
             ?: ""
 
     override fun toJql() = jql
